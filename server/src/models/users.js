8d0f985a29817f7fea21a09f6b7108d866c7f054
Nabin Karki <karki0008@gmail.com>
2024-07-17 17:08:33 +0545

    schema added

--- server/src/models/users.js
+++ server/src/models/users.js
@@ -3,8 +3,15 @@ const mongoose=require('mongoose')
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    name:String
-
+  name: String,
+  email: String,
+  password: String,
+  confirmPassword: String,
+  gender:{
+    type:String,
+    enum:['Male','Female','Others'],
+    default:'Male'
+  }
 });
 const User = mongoose.model("user", userSchema);
 module.exports=User
