96657d26de13c7c6729df19749880090dc8da8f7
Nabin Karki <karki0008@gmail.com>
2024-07-17 06:43:18 +0545

    mongodb connection setup

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { loginUser ,registerNewUser} = require("../controllers/user");
+const { loginUser ,registerNewUser} = require("../controllers/users");
 const app = Router();
 
 app.post('/register',registerNewUser);
