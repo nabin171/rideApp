04479c216bef615c43034373fd127b3b0c2d3049
Nabin Karki <karki0008@gmail.com>
2024-10-06 11:15:49 +0545

    Basic UI added

--- client/src/app/dummy/page.js
+++ client/src/app/dummy/page.js
@@ -1,27 +1,165 @@
-'use client'
-import { Button } from '@nextui-org/react'
-import React, { useState } from 'react'
+"use client";
+
+import { Button } from "@nextui-org/react";
+import React, { useState } from "react";
 
 const page = () => {
-  let [number,setNumber]=useState('');
+  let [number, setNumber] = useState("");
+  let [numberBackUp, setNumberBackUp] = useState("");
+
+  const clear = () => {
+    setNumber("");
+    setNumberBackUp(number);
+  };
 
   return (
-    <div>
+    <div className="bg-gray-500 w-[321px] ">
       {number}
       <br></br>
-      <Button onClick={() => setNumber(number + "0")}>0</Button>
-      <Button onClick={() => setNumber(number + "1")}>1</Button>
-      <Button onClick={() => setNumber(number + "2")}>2</Button>
-      <Button onClick={() => setNumber(number + "3")}>3</Button>
-      <Button onClick={() => setNumber(number + "4")}>4</Button>
-      <Button onClick={() => setNumber(number + "5")}>5</Button>
-      <Button onClick={() => setNumber(number + "6")}>6</Button>
-      <Button onClick={() => setNumber(number + "7")}>7</Button>
-      <Button onClick={() => setNumber(number + "+")}>+</Button>
-      <Button onClick={() => setNumber(number + "-")}>-</Button>
-      <Button onClick={() => setNumber(eval(number))}>=</Button>
+      <Button onClick={clear}>AC </Button>
+      <Button
+        onClick={() => {
+          setNumber(number + "⌫");
+        }}
+      >
+        ⌫{" "}
+      </Button>
+      <Button
+        onClick={() => {
+          setNumber(number + "/");
+        }}
+      >
+        /{" "}
+      </Button>
+      <Button
+        onClick={() => {
+          setNumber(number + "%");
+        }}
+      >
+        %{" "}
+      </Button>
+
+      <br></br>
+      <Button
+        onClick={() => {
+          setNumber(number + "7");
+        }}
+      >
+        7{" "}
+      </Button>
+      <Button
+        onClick={() => {
+          setNumber(number + "8");
+        }}
+      >
+        8{" "}
+      </Button>
+      <Button
+        onClick={() => {
+          setNumber(number + "9");
+        }}
+      >
+        9{" "}
+      </Button>
+      <Button
+        onClick={() => {
+          setNumber(number + "*");
+        }}
+      >
+        *{" "}
+      </Button>
+      <br></br>
+      <Button
+        onClick={() => {
+          setNumber(number + "4");
+        }}
+      >
+        4{" "}
+      </Button>
+      <Button
+        onClick={() => {
+          setNumber(number + "5");
+        }}
+      >
+        5{" "}
+      </Button>
+      <Button
+        onClick={() => {
+          setNumber(number + "6");
+        }}
+      >
+        6{" "}
+      </Button>
+      <Button
+        onClick={() => {
+          setNumber(number + "-");
+        }}
+      >
+        -{" "}
+      </Button>
+      <br></br>
+      <Button
+        onClick={() => {
+          setNumber(number + 1);
+        }}
+      >
+        1{" "}
+      </Button>
+      <Button
+        onClick={() => {
+          setNumber(number + "2");
+        }}
+      >
+        2{" "}
+      </Button>
+      <Button
+        onClick={() => {
+          setNumber(number + "3");
+        }}
+      >
+        3{" "}
+      </Button>
+
+      <Button
+        onClick={() => {
+          setNumber(number + "+");
+        }}
+      >
+        +{" "}
+      </Button>
+      <br></br>
+
+      <Button
+        onClick={() => {
+          setNumber(number + 0);
+        }}
+      >
+        0{" "}
+      </Button>
+      <Button
+        onClick={() => {
+          setNumber(number + 0);
+        }}
+      >
+        0{" "}
+      </Button>
+      <Button
+        onClick={() => {
+          setNumber(numberBackUp);
+        }}
+      >
+        ANS{" "}
+      </Button>
+      <Button
+        onClick={() => {
+          setNumber(eval(number));
+        }}
+      >
+        {" "}
+        ={" "}
+      </Button>
     </div>
   );
-}
+};
 
-export default page
+export default page;
